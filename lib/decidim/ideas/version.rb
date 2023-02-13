c6d8813347ded5bc5576dc0fa79e7cc0cc94bcce
Heiner Sameisky <hei.sam@gmail.com>
2023-02-13 21:17:24 +0100

    Update version.rb

--- lib/decidim/ideas/version.rb
+++ lib/decidim/ideas/version.rb
@@ -3,7 +3,7 @@
 module Decidim
   # This holds the decidim-ideas version.
   module Ideas
-    VERSION = '0.27.0'
-    DECIDIM_VERSION = '>= 0.27.0'
+    VERSION = '0.27.2'
+    DECIDIM_VERSION = '>= 0.27.2'
   end
 end
